3e8d81cf22fce17641497f9c6703523679e9ad89
BranaX <bjorntrends@gmail.com>
2021-05-13 22:27:29 +0545

    fahrengheit to celsius added

--- app/app.js
+++ app/app.js
@@ -1,27 +1,47 @@
 'use strict';
 
 const con = document.getElementById('convert');
+const far = document.getElementById('far');
 const submit = document.getElementById('celsius');
+const submit1 = document.getElementById('fahrenheit');
 const dispElm0 = document.getElementById('display-result-0');
+const dispElm1 = document.getElementById('display-result-1');
 
+/***** Celsius To Fahrenheit *****/
 function convertToF(celsius) {
   let fahrenheit = celsius * (9 / 5) + 32;
 
   return fahrenheit;
 }
-
 convertToF(6);
 
 con.addEventListener('click', function () {
+  console.log('click++');
   this.style.background = 'green';
   let number = submit.value;
   dispElm0.textContent = `${convertToF(number)}°F`;
 
   convertToF(number);
-
-  submit.defaultValue = 'numbers here!';
 });
 
+/*****Fahrenheit to Celsius *****/
+function convertToC(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+
+  return celsius;
+}
+convertToC(1);
+
+far.addEventListener('click', function () {
+  console.log('click2+');
+  this.style.background = '#490050';
+  let number1 = submit1.value;
+  dispElm1.textContent = `${convertToC(number1)}°C`;
+
+  convertToC(number1);
+});
+
+/***** Buttons disabled if input empty *****/
 function success() {
   if (submit.value === '') {
     con.disabled = true;
@@ -30,7 +50,13 @@ function success() {
   }
 }
 
-/***** Show Hidden *****/
+function success() {
+  if (submit1.value === '') {
+    far.disabled = true;
+  } else {
+    far.disabled = false;
+  }
+}
 
 /***** INPUT FILTER *****/
 
@@ -62,11 +88,19 @@ function setInputFilter(textbox, inputFilter) {
 /***** PRESS "ENTER" KEY TO TRIGGER BUTTON *****/
 
 let press = submit;
+let press1 = submit1;
 
 press.addEventListener('keyup', function (event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
     event.preventDefault();
-    document.getElementById('convert').click();
+    con.click();
+  }
+});
+
+press1.addEventListener('keyup', function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    far.click();
   }
 });
